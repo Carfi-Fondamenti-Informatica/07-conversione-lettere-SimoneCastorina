c40d3d963bfe08cd515599d316bd9162d8c24fb1
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-12-07 18:41:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  char word;
+cin>>word;
+ if(carattere(word)==1){
+     cout<<word<<endl;
+ }else{
+     cout<< "errore"<<endl;
+ }
+
   return 0;
 }
