75fa2e49847e9457727013bee49508f8189ae165
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-12-07 18:39:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
-
+#include "lib.h"
+bool carattere(char &a){
+    if(((int)a>=65 && (int)a<=90)||((int)a>=97 &&(int)a<=122)){
+        if((int)a<=90){
+            a=a+32;
+        }else {
+            a=a-32;
+        }
+     return true;
+    }else{
+        return false;
+    }
+}
